18fdbf54fb9daf9c230fc380856c560a516af1bf
metin-5115 <tuncbilek.51metin@gmail.com>
2023-12-10 21:28:33 +0300

    medyan, gauss ve kendi yumuşatma fonksiyonunu ekledim.

--- Hafta_5.Designer.cs
+++ Hafta_5.Designer.cs
@@ -30,63 +30,38 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox1.Size = new System.Drawing.Size(424, 382);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(474, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.Size = new System.Drawing.Size(427, 382);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(343, 332);
-            this.pictureBox3.TabIndex = 0;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(364, 370);
-            this.pictureBox4.TabIndex = 0;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Location = new System.Drawing.Point(406, 12);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(382, 370);
-            this.pictureBox5.TabIndex = 1;
-            this.pictureBox5.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(282, 397);
+            this.button1.Location = new System.Drawing.Point(342, 411);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 2;
@@ -96,13 +71,13 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(694, 397);
+            this.button2.Location = new System.Drawing.Point(807, 411);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 3;
             this.button2.Text = "uygula";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBox1
             // 
@@ -110,34 +85,46 @@
             this.comboBox1.Items.AddRange(new object[] {
             "median ",
             "mean",
-            "gauss"});
-            this.comboBox1.Location = new System.Drawing.Point(515, 398);
+            "gauss",
+            "box"});
+            this.comboBox1.Location = new System.Drawing.Point(639, 412);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(151, 28);
             this.comboBox1.TabIndex = 4;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(515, 445);
+            this.textBox1.Location = new System.Drawing.Point(639, 455);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(125, 27);
             this.textBox1.TabIndex = 5;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(807, 446);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "custom blur";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Hafta_5
             // 
-            this.ClientSize = new System.Drawing.Size(809, 578);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 619);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Hafta_5";
+            this.Text = "Hafta_5";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,12 +134,10 @@
 
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox5;
         private Button button1;
         private Button button2;
         private ComboBox comboBox1;
         private TextBox textBox1;
+        private Button button3;
     }
 }
--- Hafta_5.cs
+++ Hafta_5.cs
@@ -12,33 +12,12 @@ namespace Görüntü_İşleme
 {
     public partial class Hafta_5 : Form
     {
+
+       
+
         public Hafta_5()
         {
             InitializeComponent();
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                openFileDialog.Filter = "Resim Dosyaları|*.bmp;*.jpg;*.jpeg;*.png;*.gif|Tüm Dosyalar|*.*";
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    // Seçilen resmi PictureBox'a yükle
-                    pictureBox1.Image = new Bitmap(openFileDialog.FileName);
-                }
-            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -62,16 +41,146 @@ namespace Görüntü_İşleme
                 Bitmap gaus = gausFilter(image);
                 pictureBox2.Image = gaus;
             }
+            
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Resim Dosyaları|*.bmp;*.jpg;*.jpeg;*.png;*.gif|Tüm Dosyalar|*.*";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Seçilen resmi PictureBox'a yükle
+                    pictureBox1.Image = new Bitmap(openFileDialog.FileName);
+                }
+            }
+        }
+
+
 
         private Bitmap medianFilter(Bitmap image)
         {
-            throw new NotImplementedException();
+            Bitmap GirisResmi = new Bitmap(pictureBox1.Image);
+            int ResimGenisligi = GirisResmi.Width;
+            int ResimYuksekligi = GirisResmi.Height;
+
+            Bitmap CikisResmi = new Bitmap(ResimGenisligi, ResimYuksekligi);
+
+            Color OkunanRenk;
+
+            int SablonBoyutu = int.Parse(textBox1.Text);
+
+            int ElemanSayisi = SablonBoyutu * SablonBoyutu;
+
+            //int[,,,] Renk_Matrisi = new int[];
+            int[] R = new int[ElemanSayisi];
+
+            int[] G = new int[ElemanSayisi];
+
+            int[] B = new int[ElemanSayisi];
+
+            int[] Gri = new int[ElemanSayisi];
+
+
+            for (int x = (SablonBoyutu - 1) / 2; x < ResimGenisligi - (SablonBoyutu - 1) / 2; x++)
+            {
+                for (int y = (SablonBoyutu - 1) / 2; y < ResimYuksekligi - (SablonBoyutu - 1) / 2; y++)
+                {
+                    //Şablon bölgesi (çekirdek matris) içindeki pikselleri tarıyor.
+                    int k = 0;
+
+                    for (int i = -((SablonBoyutu - 1) / 2); i <= (SablonBoyutu - 1) / 2; i++)
+                    {
+                        for (int j = -((SablonBoyutu - 1) / 2); j <= (SablonBoyutu - 1) / 2; j++)
+                        {
+                            OkunanRenk = GirisResmi.GetPixel(x + i, y + j);
+                            R[k] = OkunanRenk.R;
+                            G[k] = OkunanRenk.G;
+                            B[k] = OkunanRenk.B;
+                            Gri[k] = Convert.ToInt16(R[k] * 0.299 + G[k] * 0.587 + B[k] * 0.114); //Gri ton formülü
+                            k++;
+                        }
+                    }
+                    
+                    
+                    //Gri tona göre sıralama yapıyor. Aynı anda üç rengide değiştiriyor.
+                    int GeciciSayi = 0;
+                    for (int i = 0; i < ElemanSayisi; i++)
+                    {
+                        for (int j = i + 1; j < ElemanSayisi; j++)
+                        {
+                            if (Gri[j] < Gri[i])
+                            {
+                                GeciciSayi = Gri[i];
+                                Gri[i] = Gri[j];
+                                Gri[j] = GeciciSayi;
+                                GeciciSayi = R[i];
+                                R[i] = R[j];
+                                R[j] = GeciciSayi;
+                                GeciciSayi = G[i];
+                                G[i] = G[j];
+                                G[j] = GeciciSayi;
+                                GeciciSayi = B[i];
+                                B[i] = B[j];
+                                B[j] = GeciciSayi;
+                            }
+                        }
+                    }
+                    //Sıralama sonrası ortadaki değeri çıkış resminin piksel değeri olarak atıyor. 
+                    CikisResmi.SetPixel(x, y, Color.FromArgb(R[(ElemanSayisi - 1) / 2], G[(ElemanSayisi - 1) / 2], B[(ElemanSayisi - 1) / 2]));
+                }
+            }
+            pictureBox2.Image = CikisResmi;
+            return CikisResmi;
         }
 
         private Bitmap gausFilter(Bitmap image)
         {
-            throw new NotImplementedException();
+            Color OkunanRenk;
+            Bitmap GirisResmi, CikisResmi;
+            GirisResmi = new Bitmap(pictureBox1.Image);
+            int ResimGenisligi = GirisResmi.Width;
+            int ResimYuksekligi = GirisResmi.Height;
+            CikisResmi = new Bitmap(ResimGenisligi, ResimYuksekligi);
+            int SablonBoyutu = 5; //Çekirdek matrisin boyutu
+            int ElemanSayisi = SablonBoyutu * SablonBoyutu;
+            int x, y, i, j, toplamR, toplamG, toplamB, ortalamaR, ortalamaG, ortalamaB;
+            int[] Matris = { 1, 4, 7, 4, 1, 4, 20, 33, 20, 4, 7, 33, 55, 33, 7, 4, 20, 33, 20, 4, 1, 4, 7, 4, 1 };
+            int MatrisToplami = 1 + 4 + 7 + 4 + 1 + 4 + 20 + 33 + 20 + 4 + 7 + 33 + 55 + 33 + 7 + 4 + 20 + 33 + 20 + 4 + 1 + 4 + 7 + 4 + 1;
+            //int[] Matris = { 1, 2, 4, 2, 1, 2, 6, 9, 6, 2, 4, 9, 16, 9, 4, 2, 6, 9, 6, 2, 1, 2, 4, 2, 1 };
+            //int MatrisToplami = 1 + 2 + 4 + 2 + 1 + 2 + 6 + 9 + 6 + 2 + 4 + 9 + 16 + 9 + 4 + 2 + 6 + 9 + 6 + 2 + 1 + 2 + 4 + 2 + 1;
+            for (x = (SablonBoyutu - 1) / 2; x < ResimGenisligi - (SablonBoyutu - 1) / 2; x++) //Resmi taramaya şablonun yarısı kadar dış kenarlardan içeride başlayacak ve bitirecek.
+
+            {
+                for (y = (SablonBoyutu - 1) / 2; y < ResimYuksekligi - (SablonBoyutu - 1) / 2; y++)
+                {
+                    toplamR = 0;
+                    toplamG = 0;
+                    toplamB = 0;
+                    //Şablon bölgesi (çekirdek matris) içindeki pikselleri tarıyor.
+                    int k = 0; //matris içindeki elemanları sırayla okurken kullanılacak. 
+                    for (i = -((SablonBoyutu - 1) / 2); i <= (SablonBoyutu - 1) / 2; i++)
+                    {
+                        for (j = -((SablonBoyutu - 1) / 2); j <= (SablonBoyutu - 1) / 2; j++)
+                        {
+                            OkunanRenk = GirisResmi.GetPixel(x + i, y + j);
+                            toplamR = toplamR + OkunanRenk.R * Matris[k];
+                            toplamG = toplamG + OkunanRenk.G * Matris[k];
+                            toplamB = toplamB + OkunanRenk.B * Matris[k];
+                            k++;
+                        }
+                    }
+                    ortalamaR = toplamR / MatrisToplami;
+                    ortalamaG = toplamG / MatrisToplami;
+                    ortalamaB = toplamB / MatrisToplami;
+                    CikisResmi.SetPixel(x, y, Color.FromArgb(ortalamaR, ortalamaG, ortalamaB));
+                }
+            }
+            pictureBox2.Image = CikisResmi;
+            return CikisResmi;
+
         }
 
         private Bitmap MeanFilter(Bitmap image)
@@ -114,21 +223,50 @@ namespace Görüntü_İşleme
 
 
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+
+        //2.sorunun cevabı
+        int red = 0, blue = 0, green = 0, alpha = 0;
+        int blurPixelCount = 0;
+        private void button3_Click(object sender, EventArgs e)
         {
+            Bitmap bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(bmp2, pictureBox1.ClientRectangle);
+            Bitmap blurimg = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+
+            for (int i = Convert.ToInt32(textBox1.Text); i < bmp2.Width - Convert.ToInt32(textBox1.Text); i++)
+            {
+
+                for (int j = Convert.ToInt32(textBox1.Text); j < bmp2.Height - Convert.ToInt32(textBox1.Text); j++)
+                {
+
+                    Color clr = new Color();
+                    for (int k = i - Convert.ToInt32(textBox1.Text); k <= i + Convert.ToInt32(textBox1.Text); k++)
+                    {
+                        for (int l = j - Convert.ToInt32(textBox1.Text); l <= j + Convert.ToInt32(textBox1.Text); l++)
+                        {
+                            red += bmp2.GetPixel(k, l).R;
+                            green += bmp2.GetPixel(k, l).G;
+                            blue += bmp2.GetPixel(k, l).B;
+
+                            blurPixelCount++;
+
+                        }
+                    }
+                    red = red / blurPixelCount;
+                    green = green / blurPixelCount;
+                    blue = blue / blurPixelCount;
+                    clr = Color.FromArgb(red, green, blue);
+                    blurimg.SetPixel(i, j, clr);
+                    red = 0;
+                    blue = 0; green = 0; alpha = 0; blurPixelCount = 0;
+
+                }
+            }
+            pictureBox2.Image = (Image)blurimg;
 
         }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Hafta_5_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        
+        //2.soru burada bitiyor
     }
+    
 }
