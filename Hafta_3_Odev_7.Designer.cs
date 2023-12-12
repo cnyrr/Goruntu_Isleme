7a8ab5c0657e5040679d357b4638e8ae3bf63628
safaksekin <92650030+safaksekin@users.noreply.github.com>
2023-12-12 19:03:12 +0300

    deneme

---
+++ Hafta_3_Odev_7.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Görüntü_İşleme
+{
+    partial class Hafta_3_Odev_7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(244, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(300, 300);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Hafta_3_Odev_7
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Name = "Hafta_3_Odev_7";
+            Text = "Hafta_3_Odev_7";
+            Load += Hafta_3_Odev_7_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Hafta_3_Odev_7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Görüntü_İşleme
+{
+    public partial class Hafta_3_Odev_7 : Form
+    {
+        public Hafta_3_Odev_7()
+        {
+            InitializeComponent();
+        }
+
+        private void Hafta_3_Odev_7_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
