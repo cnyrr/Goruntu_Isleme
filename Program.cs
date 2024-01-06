7ecabbfc82b0349ffa3ba497521dca09dfac0325
Emir Can Yarar <96978553+cnyrr@users.noreply.github.com>
2024-01-06 18:04:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Görüntü_İşleme
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Hafta_3_Odev_7());
+            Application.Run(new Ana_Ekran());
         }
     }
 }
